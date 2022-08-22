7a26ff7cbf4ad265fd26b53e492ff38582e77591
Meet Kothari <109298876+mskothari07@users.noreply.github.com>
2022-08-22 11:40:03 +0530

    required input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,18 +25,21 @@ const Contact = () => {
                 name="name"
                 placeholder="Enter your name"
                 className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                required 
               />
               <input
                 type="text"
                 name="email"
                 placeholder="Enter your email"
                 className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                 required
               />
               <textarea
                 name="message"
                 placeholder="Enter your message"
                 rows="10"
                 className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                 required
               ></textarea>
   
               <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
