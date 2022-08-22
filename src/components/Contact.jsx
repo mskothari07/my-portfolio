38d9efd33e35c2f519f023dddc8f6dd831c8d819
Meet Kothari <109298876+mskothari07@users.noreply.github.com>
2022-08-22 11:46:42 +0530

    email validation update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
                 required 
               />
               <input
-                type="text"
+                type="email"
                 name="email"
                 placeholder="Enter your email"
                 className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
