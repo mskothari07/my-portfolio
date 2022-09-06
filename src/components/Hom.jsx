c399712df398a440b3cef7b4e408d7a647546846
Meet Kothari <109298876+mskothari07@users.noreply.github.com>
2022-09-06 17:12:40 +0530

    add bzc link

--- src/components/Hom.jsx
+++ src/components/Hom.jsx
@@ -13,10 +13,14 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm Clan Fellow at Blazeclan Tech
+            I'm Clan Fellow at Blazeclan Tech 
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-           Working on different cloud technologies and serving in digital services (Full Stack) unit of Blazeclan.
+           Working on different cloud technologies and serving in digital services (Full Stack) unit of <a
+  href="https://www.blazeclan.com/"
+  >Blazeclan Technologies
+</a>
+.
           </p>
 
           <div>
