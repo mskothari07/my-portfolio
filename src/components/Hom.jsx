9a3c302a9b501eadd5383fba216641a46f88d3aa
mskothari07 <kotharimeet07@gmail.com>
2022-12-30 17:21:12 +0530

    update

--- src/components/Hom.jsx
+++ src/components/Hom.jsx
@@ -13,7 +13,8 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-6xl font-bold text-white mt-12">
-            I'm Cloud Application Devloper at Blazeclan Tech 
+            I'm Cloud Application Devloper at 
+            Blazeclan Technologies
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
            Working on different cloud technologies and serving in digital service unit of Blazeclan Technologies
