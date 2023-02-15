de5b932e6f506cd366829ff54373475f4527dd1f
mskothari07 <kotharimeet07@gmail.com>
2023-02-15 22:49:57 +0530

    website responsiveness bug fixed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -43,9 +43,10 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className=" w-full h-screen bg-gradient-to-b  from-gray-800  to-black text-white md:h-screen"
+      className=" w-full  bg-gradient-to-b  from-gray-800  to-black text-white md:h-screen"
+      
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
